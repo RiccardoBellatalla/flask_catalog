3df953cbb44b526fb51f1ee15667948c1d2424d6
RiccardoBellatalla <riccardo.bellatalla13@gmail.com>
2021-12-04 19:35:55 +0100

    added WTF forms following lesson 3 of megatutorial

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from config import Config
+
 application = Flask(__name__) #original tutorial was "app =" but AWS ElasticBeanstalk needs "application ="
+app = application
+app.config.from_object(Config)
 
 from app import routes #app here is the name of the folder (also called "package")
